3acfdf4d8493e07e334151dee4078302a532c1b1
mcvinding <mikkel.vinding@gmail.com>
2019-02-13 17:26:19 +0100

    Working towards summary stats

--- scripts/burstsummary.m
+++ scripts/burstsummary.m
@@ -17,143 +17,91 @@ subs = subs(~(strcmp('.',subs)|strcmp('..',subs)));     %Remove dots
 %% Load threshold 
 
 %% N events
-load(fullfile(dirs.megDir,subs{1},'subvals2.mat'))
-
-steps = subvals{1}.steps;
-nevent1sd = nan(length(subs), length(steps));
-nevent2sd = nan(length(subs), length(steps));
-nevent1md = nan(length(subs), length(steps));
-nevent2md = nan(length(subs), length(steps));
-
-for ii = 1:length(subs)
-    load(fullfile(dirs.megDir,subs{ii},'subvalsSD.mat'))
-    
-    for j = 1:length(steps)
-        nevent1sd(ii,:) = subvals{1}.bdat(j).cutoff;
-        nevent2sd(ii,:) = subvals{2}.n_events;
-    end
-    
-    load(fullfile(dirs.megDir,subs{ii},'subvalsMED.mat'))
-    
-    for j = 1:length(steps)
-        nevent1md(ii,:) = subvals{1}.n_events;
-        nevent2md(ii,:) = subvals{2}.n_events;
-    end
-end
-
-PDn1 = nevent1sd(PDidx,:);
-ctrln1 = nevent1sd(ctrlidx,:);
-PDn2 = nevent2sd(PDidx,:);
-ctrln2 = nevent2sd(ctrlidx,:);
-
-PDnavg1 = nanmean(PDn1)
-ctrlnavg1 = nanmean(ctrln1)
-PDnsd1 = nanstd(PDn1)
-ctrlnsd1 = nanstd(ctrln1)
-
-PDnavg2 = nanmean(PDn2)
-ctrlnavg2 = nanmean(ctrln2)
-PDnsd2 = nanstd(PDn2)
-ctrlnsd2 = nanstd(ctrln2)
-
-% plot(steps,lala,'o-')
-
-figure
-plot(steps,log(PDnavg1),'or-'); hold on
-plot(steps,log(ctrlnavg1),'ob-'); 
-plot(steps,log(PDnavg2),'xr-');
-plot(steps,log(ctrlnavg2),'xb-'); hold off
-
-figure
-plot(steps,PDnavg1,'or-'); hold on
-plot(steps,ctrlnavg1,'ob-'); 
-plot(steps,PDnavg2,'xr-');
-plot(steps,ctrlnavg2,'xb-'); hold off
-
-for i = 1:length(steps)
-    [~, pt1(i),~,t] = ttest2(PDn1(:,i),ctrln1(:,i));
-    [~, pt2(i),~,t] = ttest2(PDn2(:,i),ctrln2(:,i));
-end
-
-figure;
-plot(steps,pt1, 'b'); hold on
-plot(steps,pt2, 'r'); hold on
-
-figure
-h1 = histogram(PDn1(:,17),10); hold on
-h2 = histogram(ctrln1(:,17),10); hold off
-
-mns = [PDnavg1, ctrlnavg1];
-sds = [PDnsd1, ctrlnsd1];
-sem = [PDnsd1/sqrt(length(PD_subs)), ctrlnsd1/sqrt(length(ctrl_subs))];
-
-figure; hold on
-bar(1:2,mns,0.6,'b','grouped')
-errorbar(1:2,mns,sem*2,'r.')
-set(gca,'xtick',[])
-
-%% Event duration
-lenmean = zeros(length(subs), 1);   % subjects x number of steps
-lenmedn = zeros(length(subs), 1);   % subjects x number of steps
+nevent1 = nan(length(subs), 1);
+nevent2 = nan(length(subs), 1);
 
 for ii = 1:length(subs)
     load(fullfile(dirs.megDir,subs{ii},'subvals.mat'))
     
-    for j = 1:length(subvals.steps)
-        lenmedn(ii,j) = median(subvals.bdat{j}.evelen);
-        lenmean(ii,j) = mean(subvals.bdat{j}.evelen);
-    end
+    nevent1(ii) = subvals{1}.n_events;
+    nevent2(ii) = subvals{2}.n_events;
+end
+
+PDn1 = nevent1(PDidx);
+ctrln1 = nevent1(ctrlidx);
+PDn2 = nevent2(PDidx);
+ctrln2 = nevent2(ctrlidx);
+
+PDnavg1 = nanmean(PDn1);
+ctrlnavg1 = nanmean(ctrln1);
+PDnavg2 = nanmean(PDn2);
+ctrlnavg2 = nanmean(ctrln2);
+
+PDnsd1 = nanstd(PDn1);
+ctrlnsd1 = nanstd(ctrln1);
+PDnsd2 = nanstd(PDn2);
+ctrlnsd2 = nanstd(ctrln2);
+
+[~, pt1,~,t1] = ttest2(PDn1,ctrln1);
+[~, pt2,~,t2] = ttest2(PDn2,ctrln2);
+[~, ptPt,~,tPt] = ttest(PDn1,PDn2);
+[~, ptCt,~,tCt] = ttest(ctrln1,ctrln2);
+
+
+figure
+h1 = histogram(PDn1,10); hold on
+h2 = histogram(ctrln1,10); hold off
+
+mns = [PDnavg1, ctrlnavg1; PDnavg2, ctrlnavg2];
+sds = [PDnsd1, ctrlnsd1; PDnsd2, ctrlnsd2];
+
+%% Event duration
+lenmean1 = zeros(length(subs), 1);   % subjects x number of steps
+lenmedn1 = zeros(length(subs), 1);   % subjects x number of steps
+lenmean2 = zeros(length(subs), 1);   % subjects x number of steps
+lenmedn2 = zeros(length(subs), 1);   % subjects x number of steps
+
+for ii = 1:length(subs)
+    load(fullfile(dirs.megDir,subs{ii},'subvals.mat'))
     
-%     clear subvals
+    len1 = subvals{1}.bdat.evelen;
+    len2 = subvals{2}.bdat.evelen;
+
+    lenmedn1(ii) = median(subvals{1}.bdat.evelen);
+    lenmean1(ii) = mean(subvals{1}.bdat.evelen);
+    lenmedn2(ii) = median(subvals{2}.bdat.evelen);
+    lenmean2(ii) = mean(subvals{2}.bdat.evelen);
 end
 
 
-mean(lenmean)
-mean(lenmedn)
+mean(lenmean1)
+mean(lenmedn1)
 
-plot(steps,lenmean, 'bo-'); hold on
-plot(steps,lenmedn, 'rx-'); hold off
+PDlenmn1 = lenmedn1(PDidx);
+ctrllenmn1 = lenmedn1(ctrlidx);
+PDlenmn2 = lenmedn2(PDidx);
+ctrllenmn2 = lenmedn2(ctrlidx);
+
+PDlenavg1 = mean(PDlenmn1);
+ctrllenavg1 = mean(ctrllenmn1);
+PDlenavg2 = mean(PDlenmn2);
+ctrllenavg2 = mean(ctrllenmn2);
+
+figure; 
+subplot(1,2,1); histogram(PDlenmn1,20); hold on
+subplot(1,2,1); histogram(ctrllenmn1,20); hold off
+subplot(1,2,2); histogram(PDlenmn2,20); hold on
+subplot(1,2,2); histogram(ctrllenmn2,20); hold off
 
 
-PDlenmn = lenmean(PDidx,:);
-ctrllenmn = lenmean(ctrlidx,:);
-
-PDlenavg = mean(PDlenmn);
-ctrllenavg = mean(ctrllenmn);
-PDlensd = std(PDlenmn);
-ctrllensd = std(ctrllenmn);
-
-figure;
-plot(subvals.steps,PDlenavg,'or-'); hold on
-plot(subvals.steps,ctrllenavg,'ob-'); hold off
-
-for ii = 1:length(subvals.steps)
-    [~, pt(ii)] = ttest2(PDlenmn(:,ii),ctrllenmn(:,ii));
-end
-plot(subvals.steps,pt)
-
-% Median
-PDlenmd = lenmedn(PDidx,:);
-ctrllenmd = lenmedn(ctrlidx,:);
-
-PDlenavg = mean(PDlenmd);
-ctrllenavg = mean(ctrllenmd);
-PDlensd = std(PDlenmd);
-ctrllensd = std(ctrllenmd);
-
-figure; hold on
-plot(subvals.steps,PDlenavg,'or-');
-plot(subvals.steps,ctrllenavg,'ob-'); hold off
-
-for ii = 1:length(subvals.steps)
-    [~, pt(ii)] = ttest2(PDlenmd(:,ii),ctrllenmd(:,ii));
-end
-
-figure; plot(subvals.steps,pt)
+[~, pt1,~,t1] = ttest2(PDlenmn1,ctrllenmn1);
+[~, pt2,~,t2] = ttest2(PDlenmn2,ctrllenmn2);
+[~, ptPt,~,tPt] = ttest2(PDlenmn1,PDlenmn2);
+[~, ptCt,~,tCt] = ttest2(ctrllenmn1,ctrllenmn2);
 
 
 
-mns = [PDlenavg(17), ctrllenavg(17)];
+mns = [PDlenavg1, ctrllenavg1; ];
 sds = [PDlensd(17), ctrllensd(17)];
 sem = [PDlensd(17)/sqrt(length(PD_subs)), ctrllensd(17)/sqrt(length(ctrl_subs))];
 
@@ -187,20 +135,20 @@ figure; hold on
 plot(steps,toemean, 'bo-');
 plot(steps,toemedn, 'rx-'); hold off
 
-PDlenmn = toemedn(PDidx,:);
-ctrllenmn = toemedn(ctrlidx,:);
+PDlenmn1 = toemedn(PDidx,:);
+ctrllenmn1 = toemedn(ctrlidx,:);
 
-PDlenavg = median(PDlenmn);
-ctrllenavg = median(ctrllenmn);
-PDlensd = std(PDlenmn);
-ctrllensd = std(ctrllenmn);
+PDlenavg = median(PDlenmn1);
+ctrllenavg = median(ctrllenmn1);
+PDlensd = std(PDlenmn1);
+ctrllensd = std(ctrllenmn1);
 
 figure; hold on
 plot(subvals.steps,PDlenavg,'or-');
 plot(subvals.steps,ctrllenavg,'ob-'); hold off
 
 for ii = 1:length(subvals.steps)
-    [~, pt(ii)] = ttest2(PDlenmn(:,ii),ctrllenmn(:,ii));
+    [~, pt(ii)] = ttest2(PDlenmn1(:,ii),ctrllenmn1(:,ii));
 end
 figure; plot(subvals.steps,pt)
 
