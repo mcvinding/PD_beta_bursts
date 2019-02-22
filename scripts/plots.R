f4b7c1ec937a3532b20fab2d12472d876f074530
mcvinding <mikkel.vinding@gmail.com>
2019-02-22 19:07:48 +0100

    Update R scripts

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,2 +1,18 @@
 ## Pretty plots
 library(ggplot2)
+
+# Plot N events
+neve.data$task <- paste(neve.data$group,neve.data$session)
+n.summary <- aggregate(neve.data$nevent, list(neve.data$task), mean)
+names(n.summary) <- c("task","mean")
+n.summary.sd <- aggregate(neve.data$nevent, list(neve.data$task), sd)
+n.summary$sd <- n.summary.sd$x
+n.summary$se <- n.summary$sd/sqrt(20)*2
+
+nplt <- ggplot(neve.data, aes(x=task, y=nevent))+
+  geom_point(aes(fill=subs),position=position_jitter(width = 0.2),color='black',shape=21,size=2) +
+  geom_crossbar(data=n.summary, aes(x=task,y=mean, ymin=mean,ymax=mean), width = 0.5) +
+  geom_errorbar(data=n.summary,aes(x=task,y=mean, ymin=mean-sd, ymax=mean+sd), width=0.2) +
+  guides(fill=F) + theme_bw()
+  # ylim(225, 475)
+nplt
--- scripts/stats.R
+++ scripts/stats.R
@@ -13,13 +13,28 @@ load(file = 'workspace.Rdata')
 lme.neve <- lme(nevent ~ group*session, data=neve.data, random = ~1|subs)
 anova(lme.neve)
 
-summary(glht(lme.neve, linfct = mcp(list(session="Tukey",group="Tukey")), test = adjusted(type = "bonferroni"))
+summary(glht(lme.neve, linfct = mcp(list(session="Tukey",group="Tukey")), test = adjusted(type = "bonferroni")))
         
-aov.neve <- aov(nevent ~ group*session+Error(subs),data=neve.data))
+aov.neve <- aov(nevent ~ group*session+Error(subs),data=neve.data)
 summary(aov.neve)
 post.hoc <- TukeyHSD(aov.neve)
 
-mod <- glmer(nevent ~ group*session+(1|subs), data = neve.data, family='poisson')
+mod1 <- glmer(nevent ~ group*session+(1|subs), data = neve.data, family='poisson')
+mod2 <- update(mod1, ~. -group:session)
+mod3 <- update(mod2, ~. -group)
+mod4 <- update(mod3, ~. -session)
+
+anova(mod1,mod2,mod3,mod4)
+
+mod1 <- lmer(nevent ~ group*session+(1|subs), data = neve.data)
+mod2 <- update(mod1, ~. -group:session)
+mod3 <- update(mod2, ~. -group)
+mod4 <- update(mod3, ~. -session)
+
+anova(mod1,mod2,mod3,mod4)
 
 b <- anovaBF(nevent ~ group*session+subs, whichRandom = "subs", data = neve.data)
+summary(b)
+plot(b)
 
+brmod <- brm(bf(nevent ~ group), data = neve.data, family = hurdle_poisson())
