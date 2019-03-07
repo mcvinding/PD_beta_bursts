cedf30bb02de63be08eface1a4f56f5b0e6d06ae
mcvinding <mikkel.vinding@gmail.com>
2019-03-07 20:36:09 +0100

    Testing models

--- scripts/stats.R
+++ scripts/stats.R
@@ -9,7 +9,7 @@ Sys.setenv(BINPREF = "C:/Rtools/mingw_$(WIN)/bin/")
 
 # Define paths
 wrkdir <- "Z://PD_motor//rest_ec//groupanalysis//"
-wrkdir <- "C://Users//Mikkel//Documents//PD-proj_betaEvent//data"
+# wrkdir <- "C://Users//Mikkel//Documents//PD-proj_betaEvent//data"
 setwd(wrkdir)
 load(file = 'workspace.Rdata')
 load(file = '.Rdata')
@@ -42,35 +42,43 @@ b <- anovaBF(nevent ~ session*group+subs, whichRandom = "subs", data = neve.data
 summary(b)
 plot(b)
 
-br.nev3 <- brm(bf(nevent ~ group*session+(1|subs)), data = neve.data, family = poisson, save_all_pars = TRUE)
-br.nev2 <- brm(bf(nevent ~ group+session+(1|subs)), data = neve.data, family = poisson, save_all_pars = TRUE)
-br.nev1 <- brm(bf(nevent ~ session+(1|subs)), data = neve.data, family = poisson, save_all_pars = TRUE)
-br.nev0 <- brm(bf(nevent ~ 1+(1|subs)), data = neve.data, family = poisson, save_all_pars = TRUE)
+br.nev3 <- brm(bf(nevent ~ group*session+(1|subs)), data = neve.data, family = poisson, 
+               save_all_pars = TRUE, iter = 5000, cores = 3)
+br.nev2 <- brm(bf(nevent ~ group+session+(1|subs)), data = neve.data, family = poisson, 
+               save_all_pars = TRUE, iter = 5000, cores = 3)
+br.nev1 <- brm(bf(nevent ~ session+(1|subs)), data = neve.data, family = poisson, 
+               save_all_pars = TRUE, iter = 5000, cores = 3)
+br.nev0 <- brm(bf(nevent ~ 1+(1|subs)), data = neve.data, family = poisson, 
+               save_all_pars = TRUE, iter = 5000)
 
-bf10 <- bayes_factor(br.nev1,br.nev0)
-bf21 <- bayes_factor(br.nev2,br.nev1)
-bf32 <- bayes_factor(br.nev3,br.nev2)
+n.bf10 <- bayes_factor(br.nev1,br.nev0)
+n.bf21 <- bayes_factor(br.nev2,br.nev1)
+n.bf32 <- bayes_factor(br.nev3,br.nev2)
+n.bf31 <- bayes_factor(br.nev3,br.nev1)
 
-bf10
-bf21
-bf32
+n.bf10
+n.bf21
+n.bf32
+n.bf31
 
-br.nev3ga <- brm(bf(nevent ~ group*session+(1|subs)), 
-                 data = neve.data, family = gaussian, save_all_pars = TRUE)
-br.nev2ga <- brm(bf(nevent ~ group+session+(1|subs)), 
-                 data = neve.data, family = gaussian, save_all_pars = TRUE)
-br.nev1ga <- brm(bf(nevent ~ session+(1|subs)), 
-                 data = neve.data, family = gaussian, save_all_pars = TRUE)
-br.nev0ga <- brm(bf(nevent ~ 1+(1|subs)), 
-                 data = neve.data, family = gaussian, save_all_pars = TRUE)
+br.nev3ga <- brm(bf(nevent ~ group*session+(1|subs)), data = neve.data, family = gaussian,
+                 save_all_pars = TRUE, iter = 5000, cores = 3)
+br.nev2ga <- brm(bf(nevent ~ group+session+(1|subs)), data = neve.data, family = gaussian,
+                 save_all_pars = TRUE, iter = 5000, cores = 3)
+br.nev1ga <- brm(bf(nevent ~ session+(1|subs)), data = neve.data, family = gaussian, 
+                 save_all_pars = TRUE, iter = 5000, cores = 3)
+br.nev0ga <- brm(bf(nevent ~ 1+(1|subs)), data = neve.data, family = gaussian, 
+                 save_all_pars = TRUE, iter = 5000, cores = 3)
 
-bf10ga <- bayes_factor(br.nev1ga,br.nev0ga)
-bf21ga <- bayes_factor(br.nev2ga,br.nev1ga)
-bf32ga <- bayes_factor(br.nev3ga,br.nev2ga)
+n.bf10ga <- bayes_factor(br.nev1ga,br.nev0ga)
+n.bf21ga <- bayes_factor(br.nev2ga,br.nev1ga)
+n.bf32ga <- bayes_factor(br.nev3ga,br.nev2ga)
+n.bf31ga <- bayes_factor(br.nev3ga,br.nev1ga)
 
-bf10ga
-bf21ga
-bf32ga
+n.bf10ga
+n.bf21ga
+n.bf32ga
+n.bf31ga
 
 summary(br.nev3)
 summary(br.nev3ga)
@@ -88,6 +96,8 @@ summary(iti.mod3)
 br.iti <- brm(bf(eve.iti.ms ~ group*session+(1|subs)), data = itieve.data, family = lognormal())
 
 ## Event length
+load(file = 'leneve.RData')
+
 leneve.data$log.eve.len <- log(leneve.data$eve.len)
 len.mod3 <- lmer(eve.len.ms ~ group*session+(1|subs), data = leneve.data, REML = F)
 len.mod2 <- lmer(eve.len.ms ~ group+session+(1|subs), data = leneve.data, REML = F)
@@ -97,14 +107,18 @@ len.mod0 <- lmer(eve.len.ms ~ 1+(1|subs), data = leneve.data, REML = F)
 anova(len.mod0,len.mod1,len.mod2,len.mod3)
 summary(len.mod3)
 
-br.len3 <- brm(bf(eve.len.ms ~ group*session+(1|subs)), 
-               data = leneve.data, family = lognormal, save_all_pars=T)
-br.len2 <- brm(bf(eve.len.ms ~ group+session+(1|subs)), 
-               data = leneve.data, family = lognormal, save_all_pars=T)
-br.len1 <- brm(bf(eve.len.ms ~ session+(1|subs)), 
-               data = leneve.data, family = lognormal, save_all_pars=T)
-br.len0 <- brm(bf(eve.len.ms ~ 1+(1|subs)), 
-               data = leneve.data, family = lognormal, save_all_pars=T)
+br.len3 <- brm(bf(eve.len.ms ~ group*session+(1|subs)),
+               data = leneve.data, family = lognormal,
+               save_all_pars = TRUE, iter = 5000, cores = 3)
+br.len2 <- brm(bf(eve.len.ms ~ group+session+(1|subs)),
+               data = leneve.data, family = lognormal, 
+               save_all_pars = TRUE, iter = 5000, cores = 3)
+br.len1 <- brm(bf(eve.len.ms ~ session+(1|subs)),
+               data = leneve.data, family = lognormal,
+               save_all_pars = TRUE, iter = 5000, cores = 3)
+br.len0 <- brm(bf(eve.len.ms ~ 1+(1|subs)),
+               data = leneve.data, family = lognormal, 
+               save_all_pars = TRUE, iter = 5000, cores = 3)
 
 len.bf10 <- bayes_factor(br.len1,br.len0)
 len.bf21 <- bayes_factor(br.len2,br.len1)
@@ -112,6 +126,15 @@ len.bf32 <- bayes_factor(br.len3,br.len2)
 len.bf10
 len.bf21
 len.bf32
+
+br.len0sn <- brm(bf(eve.len.ms ~ 1+(1|subs)),
+               data = leneve.data, family = skew_normal,
+               save_all_pars = TRUE, iter = 2000, cores = 3)
+br.len0sn <- brm(bf(eve.len.ms ~ 1+(1|subs)),
+                 data = leneve.data, family = shifted_lognormal,
+                 save_all_pars = TRUE, iter = 2000, cores = 3)
+
+
 
 ## Max power
 br.max3 <- brm(bf(eve.max ~ group*session+(1|subs)), 
@@ -127,23 +150,35 @@ max.bf10 <- bayes_factor(br.max1,br.max0)
 max.bf21 <- bayes_factor(br.max2,br.max1)
 max.bf32 <- bayes_factor(br.max3,br.max2)
 
-br.max3gau <- brm(bf(eve.max ~ group*session+(1|subs)), 
-               data = maxeve.data, family = gaussian, save_all_pars=T)
-br.max2gau <- brm(bf(eve.max ~ group+session+(1|subs)), 
-               data = maxeve.data, family = gaussian, save_all_pars=T)
-br.max1gau <- brm(bf(eve.max ~ session+(1|subs)), 
-               data = maxeve.data, family = gaussian, save_all_pars=T)
-br.max0gau <- brm(bf(eve.max ~ 1+(1|subs)), 
-               data = maxeve.data, family = gaussian, save_all_pars=T)
-
-max.bf10.gau <- bayes_factor(br.max1gau,br.max0gau)
-max.bf21.gau <- bayes_factor(br.max2gau,br.max1gau)
-max.bf32.gau <- bayes_factor(br.max3gau,br.max2gau)
+# Not good model
+# br.max3gau <- brm(bf(eve.max ~ group*session+(1|subs)), 
+#                data = maxeve.data, family = gaussian, save_all_pars=T)
+# br.max2gau <- brm(bf(eve.max ~ group+session+(1|subs)), 
+#                data = maxeve.data, family = gaussian, save_all_pars=T)
+# br.max1gau <- brm(bf(eve.max ~ session+(1|subs)), 
+#                data = maxeve.data, family = gaussian, save_all_pars=T)
+# br.max0gau <- brm(bf(eve.max ~ 1+(1|subs)), 
+#                data = maxeve.data, family = gaussian, save_all_pars=T)
+# 
+# max.bf10.gau <- bayes_factor(br.max1gau,br.max0gau)
+# max.bf21.gau <- bayes_factor(br.max2gau,br.max1gau)
+# max.bf32.gau <- bayes_factor(br.max3gau,br.max2gau)
 
 maxeve.data$log.eve.max <- log(maxeve.data$eve.max)
 
 br.max3ln <- brm(bf(log.eve.max ~ group*session+(1|subs)), 
                data = maxeve.data, family = gaussian, save_all_pars=T)
+br.max2ln <- brm(bf(log.eve.max ~ group+session+(1|subs)), 
+                 data = maxeve.data, family = gaussian, save_all_pars=T)
+br.max1ln <- brm(bf(log.eve.max ~ session+(1|subs)), 
+                 data = maxeve.data, family = gaussian, save_all_pars=T)
+br.max0ln <- brm(bf(log.eve.max ~ 1+(1|subs)), 
+                 data = maxeve.data, family = gaussian, save_all_pars=T)
+
+max.bf10.ln <- bayes_factor(br.max1ln,br.max0ln)
+max.bf21.ln <- bayes_factor(br.max2ln,br.max1ln)
+max.bf32.ln <- bayes_factor(br.max3ln,br.max2ln)
+
 
 
 save.image(file='workspace.Rdata')
